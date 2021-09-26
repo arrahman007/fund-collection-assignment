753ecd416b0c27c1c4fda5f82438115aaf940652
arrahman007 <a.arrahaman007@gmail.com>
2021-09-26 08:40:58 +0600

    adding data

---
+++ public/data.js
@@ -0,0 +1,92 @@
+const schoolData = [
+    {
+        name: 'DAKSHIN KATHALIA TAZEM ALI SECONDARY SCHOOL',
+        totalStudent: '685',
+        image: <img src="#" alt="" />,
+        headMasterName: 'Wadudur Rahman',
+        address: 'Kathalia, Amtali, Barguna',
+        fundAmount: 100000
+
+    },
+    {
+        name: 'LOCHA JUUNIOR HIGH SCHOOL',
+        totalStudent: '452',
+        image: <img src="#" alt="" />,
+        headMasterName: 'Mahbubur Rahman',
+        address: 'Amtali, Barguna',
+        fundAmount: 100000
+
+    },
+    {
+        name: 'BAROHAL EAHIA HIGH SCHOOL',
+        totalStudent: '350',
+        image: <img src="#" alt="" />,
+        headMasterName: 'Abdus Sattar',
+        address: 'Shahgali Bazar Rd, Barohal Union',
+        fundAmount: 100000
+
+    },
+    {
+        name: 'BANOIL HIGH SCHOOL',
+        totalStudent: '145',
+        image: <img src="#" alt="" />,
+        headMasterName: 'Iqbal Mahmud',
+        address: 'Banoil Baghmara Rajshahi',
+        fundAmount: 100000
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    },
+    {
+        name: '#',
+        totalStudent: '#',
+        image: <img src="#" alt="" />,
+        headMasterName: '#',
+        address: '#',
+        fundAmount: 0
+
+    }
+]
